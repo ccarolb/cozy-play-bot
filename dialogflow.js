33bda18dd013c6dc50b64218d066c81173c08798
Carolina Borges <carolinaborgescardoso@gmail.com>
2021-12-17 16:30:03 -0300

    feat: implementação do request e response dialogflow

--- dialogflow.js
+++ dialogflow.js
@@ -10,3 +10,23 @@ const sessionClient = new dialogflow.SessionsClient({
         client_email: configs.client_email
     }
 })
+
+
+async function sendMessage(chatId, message) {
+    const sessionPath = sessionClient.sessionPath(configs.project_id, chatId);
+
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: message,
+                languageCode: 'pt-BR',
+            },
+        },
+    }
+    const responses = await sessionClient.detectIntent(request);
+    const result = responses[0].queryResult;
+    console.log(result);
+}
+
+sendMessage('123214', 'oi')
